264192a21f00e5225381f92201f83f9e86c418da
Jakarin Chujan <jojogamer67@gmail.com>
2021-11-03 06:06:24 +0700

    Complete the code for logging_test(logger).

--- logging_demo.py
+++ logging_demo.py
@@ -9,22 +9,21 @@ For details, see: https://docs.python.org/3/library/logging.html
 """
 import logging
 
+
 def logging_test(logger):
     """Log messages using each of the standard logging levels 
        plus 1 custom log level.
-    """ 
-    # TODO write a log message that uses each of these log levels.
-    #  Your log message should be an example of the sort of information 
-    #  you would log at that level:
-    #
-    # debug
-    # info
-    # warning
-    # level = logging.WARN + 5 (custom log level between WARN and ERROR)
-    # error
-    # critical or fatal
+    """
     level = logging.WARN + 5  # custom log level
-    print("You forgot to write logging_test")
+    logging.addLevelName(level, 'LEVEL')
+    assert logging.getLevelName(level) == 'LEVEL'
+
+    logger.debug(logging.DEBUG)
+    logger.info(logging.INFO)
+    logger.warning(logging.WARN)
+    logger.log(level=level, msg=level)
+    logger.error(logging.ERROR)
+    logger.critical(logging.CRITICAL)
 
 
 def simple_config():
@@ -77,7 +76,5 @@ if __name__ == "__main__":
     print("Logging to ", str(logger))
     logging_test(logger)
 
-
     # TODO create a named logger, set a a custom log threshold,
     #       and call logging_test again with your named logger.
-
